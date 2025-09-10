7113ab37ae31ee38237dc02c126296332327c90d
pavithrakd790-dev <pavithrakd790@gmail.com>
2025-09-10 16:28:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>49348ed3 </title>
-  <?php require_once "bootstrap.php"; ?>
+<title>Pavithra Devaiah (49348ed3) - Autos Database</title>
+<?php include "bootstrap.php"; ?>
 </head>
 <body>
-  <div class="container">
-    <h1>Welcome to Autos Database</h1>
-    <p><strong>Note:</strong> This sample code is only partially done and serves only as a starting point for the assignment.</p>
-    <p>
-      <a href="login.php">Please Log In</a>
-    </p>
-    <p>
-      Attempt to go to <a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
-    </p>
-    <p>
-      <a href="http://www.wa4e.com/code/rps.zip">Specification for this assignment</a>
-    </p>
-  </div>
+<div class="container">
+<h1>Welcome to Autos Database</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves only as a starting point for the assignment.
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to
+<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+<p>
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Source Code for this Application</a>
+</p>
+</div>
 </body>
-</html>
